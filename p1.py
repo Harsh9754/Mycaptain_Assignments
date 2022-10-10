c9730995b67707649d91669eb0253930a12aa446
Harsh Patidar <115228305+Harsh9754@users.noreply.github.com>
2022-10-10 17:13:30 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 17:09:53 2022
+
+@author: harshmanishpatidar
+"""
+
+r = float(input("Enter radius of circle:"))
+
+a = 3.14159 * r * r;
+
+print("Area of circle =", a)
